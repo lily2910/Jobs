5f3dbbf8339a8c00a7aeebaecb09cba82330891c
lily2909 <290965350@qq.com>
2017-08-18 17:06:22 +0800

    小利
    资源更新 刚才传错了

---
+++ job-parent/src/main/java/FY_job/job_parent/App.java
@@ -0,0 +1,13 @@
+package FY_job.job_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
